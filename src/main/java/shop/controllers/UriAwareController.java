dc1ee25557a2c34af1d16523990ed5eee16875c1
AlexT364 <sasssha364@mail.ru>
2025-08-29 22:27:24 +0300

    docs(UriAwareController): edited javadoc for controller

--- src/main/java/shop/controllers/UriAwareController.java
+++ src/main/java/shop/controllers/UriAwareController.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 /**
  * As of right now, this abstract controller returns request URI, that is used to further stylize navigation links inside page header.
  * May be changed in the future.
- * Inherited by: ShopController, HomeController, ContactController, AuthorViewController, CartController
+ * Inherited by: ShopController, HomeController, ContactController, AuthorViewController, CartController, SalesController
  */
 public abstract class UriAwareController {
 	
