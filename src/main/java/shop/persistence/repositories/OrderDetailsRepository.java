b3c5607ece096762385c2c9187dbbd61c33b3e68
AlexT364 <sasssha364@mail.ru>
2025-08-29 22:35:26 +0300

    refactor(orderdetails): add custom queries with EntityGraph in repository

--- src/main/java/shop/persistence/repositories/OrderDetailsRepository.java
+++ src/main/java/shop/persistence/repositories/OrderDetailsRepository.java
@@ -1,11 +1,30 @@
 package shop.persistence.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import shop.persistence.entities.Order;
 import shop.persistence.entities.OrderDetails;
 import shop.persistence.entities.embeddables.OrderDetailsPK;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, OrderDetailsPK>{
 	
-	
+	@EntityGraph(attributePaths = {"book", "order"})
+	@Query("""
+			SELECT od
+			FROM OrderDetails od
+			WHERE od.order.id = :orderId
+			""")
+	List<OrderDetails> findAllByOrderId(Long orderId);
+
+	@EntityGraph(attributePaths = {"order"})
+	@Query("""
+			SELECT od 
+			FROM OrderDetails od
+			WHERE od.order in :orders
+			""")
+	List<OrderDetails> findDetailsForOrders(List<Order> orders);
 }
