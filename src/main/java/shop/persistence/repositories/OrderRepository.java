01f7f55376c9fe88e6983f77b84b9c42253df891
AlexT364 <sasssha364@mail.ru>
2025-08-29 22:37:33 +0300

    refactor(order): restructure queries in OrderRepository

--- src/main/java/shop/persistence/repositories/OrderRepository.java
+++ src/main/java/shop/persistence/repositories/OrderRepository.java
@@ -1,9 +1,11 @@
 package shop.persistence.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,15 +15,28 @@ import shop.persistence.entities.Order;
 public interface OrderRepository extends JpaRepository<Order, Long>{
 	
 	@Query("""
-			SELECT o FROM Order o 
-			JOIN FETCH o.user u 
-			WHERE u.username = :username
-			ORDER BY o.orderDate desc
+			   SELECT o.id FROM Order o
+			   WHERE o.user.username = :username
 			""")
-	public Page<Order> findUsersOrders(Pageable pageable, String username);
-
+	public Page<Long> findOrderIds(Pageable pageable, String username);
+		
 	@Query("""
-			SELECT o FROM Order o JOIN FETCH o.user u WHERE o.user.username = :username AND o.id = :orderId
+			SELECT o.id FROM Order o
+			""")
+	public Page<Long> findAllIds(Pageable pageable);
+	
+	@EntityGraph(attributePaths = {"orderDetails"})
+	@Query("""
+			   SELECT o from Order o
+			   WHERE o.id in :ids
+			   ORDER BY o.orderDate DESC
+			""")
+	public List<Order> findOrdersWithDetailsByIds(List<Long> ids);
+	
+	@EntityGraph(attributePaths = {"orderDetails", "user"})
+	@Query("""
+			SELECT o FROM Order o 
+			WHERE o.user.username = :username AND o.id = :orderId
 			""")
 	public Optional<Order> findByIdAndUsername(Long orderId, String username);
 
@@ -31,12 +46,6 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
 	public Page<Order> getAll(Pageable byOrderDateDesc);
 
 	@Query("""
-			SELECT o FROM Order o 
-			JOIN FETCH o.user u
-			""")
-	public Page<Order> findAll(Pageable pageable);
-	
-	@Query("""
 			SELECT o FROM Order o
 			JOIN FETCH o.user u
 			JOIN FETCH o.orderDetails od
@@ -44,5 +53,6 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
 			WHERE o.id = :orderId
 			""")
 	public Optional<Order> findById(Long orderId);
+
 	
 }
