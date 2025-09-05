5e6439f2458aa2210ec23e62756d61186bee0c17
AlexT364 <sasssha364@mail.ru>
2025-09-05 12:31:21 +0300

    refactor(test): add @ActivePropfiles('test') to test

--- src/test/java/shop/BookStoreApplicationTests.java
+++ src/test/java/shop/BookStoreApplicationTests.java
@@ -2,8 +2,10 @@ package shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookStoreApplicationTests {
 
 	@Test
